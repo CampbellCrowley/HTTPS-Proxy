f96e8098745eaf41248d0ba91cc18c65cb97817c
Campbell Crowley <github@campbellcrowley.com>
2019-11-02 16:29:51 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# HTTPS Proxy
+This is a proxy for incoming HTTPS requests to handle updating TLS certificates
+from Let's Encrypt. This proxies all HTTPS requests on port 443 to localhost
+ports 80 as HTTP requests.
+
+By default the requested domain will also be prepended to the requested path.
+
+## Header Modifications
+If `X-Forwarded-For` is set in the received request, the same value will be sent
+with the proxied request. Otherwise, `request.connection.remoteAddress` or
+`request.socket.remoteAddress` will be set.
+
+`Queries` will be set to the entire string that follows the first `?` in the
+URL. This is for compatibility with my other projects.
+
+All other headers will be forwarded as-is with the proxied request.
+
+## CLI Arguments
+`--in=443` can be used to change the listening port.  
+`--out=80` can be used to change the destination port.
+
+`--disable-domain-prepend` will disable prepending the requested domain to the
+requested URL path.
+
+`--disable-header-queries` will disable setting the `Queries` header.
+
+`--dir=/etc/letsencrypt/live/kamino.spikeybot.com/` can be used to change the
+default path to search for keys and certs.
+
+If `--dir` is not specified, `--key=./server.key`, `--cert=./server.crt`, and
+`--ca=./chain.pem` can be specified instead. They each may be specified multiple
+times, once for each file if necessary.  
+Each of these arguments correspond to the NodeJS
+[https.createServer](https://nodejs.org/api/https.html#https_https_createserver_options_requestlistener)
+options.
+
+If `--dir` is used, the proxy will attempt to re-load the cert and key files
+when they update. This will not happen with `--key`, `--cert`, or `--ca`.
+
+This can attempt to also manage Let's Encrypt Certbot verification requests via
+HTTP. This means we will attempt to bind to `--verification-port=##`, and serve
+files from `./.well-known/` as they are requested. It is recommended to use
+`--out=80` to change the output forwarding port in this case.
---
+++ index.js
@@ -0,0 +1,183 @@
+// Copyright 2019 Campbell Crowley. All rights reserved.
+// Author: Campbell Crowley (web@campbellcrowley.com)
+
+let common;
+try {
+  common = require('../../common.js');
+  common.begin(
+      process.argv.includes('--test'), !process.argv.includes('--dev'));
+} catch (err) {
+  common = {
+    log: console.log,
+    logDebug: console.log,
+    logWarning: console.log,
+    error: console.error,
+  };
+}
+const fs = require('fs');
+const path = require('path');
+const httpProxy = require('http-proxy');
+const https = require('https');
+let server;
+let watchFile = null;
+
+const pathArg = process.argv.find((el) => el.startsWith('--dir='));
+const keyPath = pathArg && pathArg.split('=').splice(1).join('=');
+if (keyPath) {
+  watchFile = `${keyPath}privkey.pem`;
+  server = https.createServer({
+    key: fs.readFileSync(`${keyPath}privkey.pem`),
+    cert: fs.readFileSync(`${keyPath}cert.pem`),
+    ca: [
+      fs.readFileSync(`${keyPath}chain.pem`),
+      fs.readFileSync(`${keyPath}fullchain.pem`),
+    ],
+  });
+} else {
+  const opts = {};
+  const keys = ['key', 'cert', 'ca'];
+  for (const k of keys) {
+    const tmp = process.argv.filter((el) => el.startsWith(`--${k}=`));
+    if (tmp && tmp.length > 0) {
+      opts[k] = [];
+      tmp.forEach((el) => {
+        const fn = el.split('=').slice(1).join('=');
+        opts[k].push(fs.readFileSync(fn));
+      });
+    }
+  }
+  server = https.createServer(opts);
+}
+let timeout = null;
+if (watchFile) {
+  // Watch the cert file, and update current certificates if they change. This
+  // is kinda a hack and isn't rechnically supported.
+  fs.watch(watchFile, () => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      common.log('Swapping certs!!!!');
+      server._sharedCreds.context.setCert(
+          fs.readFileSync(`${keyPath}cert.pem`));
+      server._sharedCreds.context.setKey(
+          fs.readFileSync(`${keyPath}privkey.pem`));
+    }, 1000);
+  });
+}
+const inArg = process.argv.find((el) => el.startsWith('--in='));
+const outArg = process.argv.find((el) => el.startsWith('--out='));
+const listenPort = inArg ? inArg.split('=').slice(1).join('=') * 1 : 443;
+const outputPort = outArg ? outArg.split('=').slice(1).join('=') * 1 : 80;
+
+const noPrepend = process.argv.find((el) => el === '--disable-domain-prepend');
+const noQueries = process.argv.find((el) => el === '--disable-header-queries');
+
+server.on('request', (req, res) => {
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress ||
+      req.socket.remoteAddress || 'ERRR';
+
+  const headers = {'x-forwarded-for': ip};
+  let queries = '';
+  if (req.url.indexOf('?') >= 0) {
+    const split = req.url.split('?');
+    req.url = split[0];
+    queries = split[1];
+  }
+  if (!noQueries) {
+    headers.queries = queries;
+  }
+  if (!noPrepend) {
+    let subDomain = 'kamino.spikeybot.com';
+    if (req.headers.host) {
+      subDomain = req.headers.host;
+    }
+    req.url = `/${subDomain}${req.url}`;
+  }
+  common.log(`  : ${req.headers.host}${req.url}`, ip);
+  proxy.web(
+      req, res, {
+        target: {host: 'localhost', port: outputPort},
+        headers: headers,
+      },
+      (e) => {
+        if (e) {
+          common.error('Error in proxying request: ' + req.url, ip);
+          console.error(e);
+        }
+      });
+});
+server.on('upgrade', (req, socket, head) => {
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress ||
+      req.socket.remoteAddress || 'ERRR';
+
+  const headers = {'x-forwarded-for': ip};
+  let queries = '';
+  if (req.url.indexOf('?') >= 0) {
+    const split = req.url.split('?');
+    req.url = split[0];
+    queries = split[1];
+  }
+  if (!noQueries) {
+    headers.queries = queries;
+  }
+  if (!noPrepend) {
+    let subDomain = 'kamino.spikeybot.com';
+    if (req.headers.host) {
+      subDomain = req.headers.host;
+    }
+    req.url = `/${subDomain}${req.url}`;
+  }
+  common.log(`WS: ${req.headers.host}${req.url}`, ip);
+  proxy.ws(req, socket, head, {
+    target: {host: 'localhost', port: outputPort},
+    headers: headers,
+  });
+});
+server.on('error', (e) => common.error(e));
+common.logDebug(`HTTPS Listening on port ${listenPort}`);
+common.logDebug(`Proxying to port ${outputPort}`);
+server.listen(listenPort, '::');
+const proxy = httpProxy.createProxyServer({ws: true});
+proxy.on('error', (e) => {
+  common.error(e.message);
+  if (e.message.indexOf('ENOTFOUND') == -1) console.log(e);
+});
+
+const portArg =
+    process.argv.find((el) => el.startsWith('--verification-port='));
+const verifListenPort = portArg && portArg.split('=').slice(1) * 1;
+if (portArg) {
+  common.logDebug(`Handling verification on port ${verifListenPort}`);
+  const http = require('http');
+  const verifServer = http.createServer();
+  verifServer.on('request', (req, res) => {
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress ||
+        req.socket.remoteAddress || 'ERRR';
+    const filename = path.normalize(`.${req.url}`);
+    if (filename.startsWith('.well-known/')) {
+      fs.readFile(filename, (err, contents) => {
+        if (!err) {
+          common.log(`SSL Verification (HTTP) (${filename})`, ip);
+          res.setHeader('Content-Length', contents.length);
+          res.setHeader('Content-Type', 'text/html');
+          res.writeHead(200);
+          res.end(contents);
+        } else if (err.code === 'ENOENT') {
+          common.log(`SSL Verification 404 (HTTP) (${filename})`, ip);
+          res.writeHead(404);
+          res.end('404');
+        } else {
+          common.error(`Failed to read file (HTTP) (${filename})`, ip);
+          console.error(err);
+          res.writeHead(500);
+          res.end(
+              '500 Internal Server Error. Failed to read file that ' +
+              'exists.');
+        }
+      });
+    } else {
+      res.writeHead(204);
+      res.end();
+    }
+  });
+  verifServer.listen(verifListenPort);
+}
