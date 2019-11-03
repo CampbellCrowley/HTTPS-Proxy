a12a4479b9b8f95d803245936222c086c070d098
Campbell Crowley <github@campbellcrowley.com>
2019-11-02 17:40:16 -0700

    Allow verification server to start without https certs

--- index.js
+++ index.js
@@ -24,29 +24,39 @@ let watchFile = null;
 const pathArg = process.argv.find((el) => el.startsWith('--dir='));
 const keyPath = pathArg && pathArg.split('=').splice(1).join('=');
 if (keyPath) {
-  watchFile = `${keyPath}privkey.pem`;
-  server = https.createServer({
-    key: fs.readFileSync(`${keyPath}privkey.pem`),
-    cert: fs.readFileSync(`${keyPath}cert.pem`),
-    ca: [
-      fs.readFileSync(`${keyPath}chain.pem`),
-      fs.readFileSync(`${keyPath}fullchain.pem`),
-    ],
-  });
-} else {
-  const opts = {};
-  const keys = ['key', 'cert', 'ca'];
-  for (const k of keys) {
-    const tmp = process.argv.filter((el) => el.startsWith(`--${k}=`));
-    if (tmp && tmp.length > 0) {
-      opts[k] = [];
-      tmp.forEach((el) => {
-        const fn = el.split('=').slice(1).join('=');
-        opts[k].push(fs.readFileSync(fn));
-      });
-    }
+  try {
+    server = https.createServer({
+      key: fs.readFileSync(`${keyPath}privkey.pem`),
+      cert: fs.readFileSync(`${keyPath}cert.pem`),
+      ca: [
+        fs.readFileSync(`${keyPath}chain.pem`),
+        fs.readFileSync(`${keyPath}fullchain.pem`),
+      ],
+    });
+    watchFile = `${keyPath}privkey.pem`;
+  } catch (err) {
+    common.error('Failed to start HTTPS server.');
+    console.error(err);
   }
-  server = https.createServer(opts);
+} else {
+  try {
+    const opts = {};
+    const keys = ['key', 'cert', 'ca'];
+    for (const k of keys) {
+      const tmp = process.argv.filter((el) => el.startsWith(`--${k}=`));
+      if (tmp && tmp.length > 0) {
+        opts[k] = [];
+        tmp.forEach((el) => {
+          const fn = el.split('=').slice(1).join('=');
+          opts[k].push(fs.readFileSync(fn));
+        });
+      }
+    }
+    server = https.createServer(opts);
+  } catch (err) {
+    common.error('Failed to start HTTPS server.');
+    console.error(err);
+  }
 }
 let timeout = null;
 if (watchFile) {
@@ -71,76 +81,78 @@ const outputPort = outArg ? outArg.split('=').slice(1).join('=') * 1 : 80;
 const noPrepend = process.argv.find((el) => el === '--disable-domain-prepend');
 const noQueries = process.argv.find((el) => el === '--disable-header-queries');
 
-server.on('request', (req, res) => {
-  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress ||
-      req.socket.remoteAddress || 'ERRR';
+if (server) {
+  server.on('request', (req, res) => {
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress ||
+        req.socket.remoteAddress || 'ERRR';
 
-  const headers = {'x-forwarded-for': ip};
-  let queries = '';
-  if (req.url.indexOf('?') >= 0) {
-    const split = req.url.split('?');
-    req.url = split[0];
-    queries = split[1];
-  }
-  if (!noQueries) {
-    headers.queries = queries;
-  }
-  if (!noPrepend) {
-    let subDomain = 'kamino.spikeybot.com';
-    if (req.headers.host) {
-      subDomain = req.headers.host;
+    const headers = {'x-forwarded-for': ip};
+    let queries = '';
+    if (req.url.indexOf('?') >= 0) {
+      const split = req.url.split('?');
+      req.url = split[0];
+      queries = split[1];
     }
-    req.url = `/${subDomain}${req.url}`;
-  }
-  common.log(`  : ${req.headers.host}${req.url}`, ip);
-  proxy.web(
-      req, res, {
-        target: {host: 'localhost', port: outputPort},
-        headers: headers,
-      },
-      (e) => {
-        if (e) {
-          common.error('Error in proxying request: ' + req.url, ip);
-          console.error(e);
-        }
-      });
-});
-server.on('upgrade', (req, socket, head) => {
-  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress ||
-      req.socket.remoteAddress || 'ERRR';
-
-  const headers = {'x-forwarded-for': ip};
-  let queries = '';
-  if (req.url.indexOf('?') >= 0) {
-    const split = req.url.split('?');
-    req.url = split[0];
-    queries = split[1];
-  }
-  if (!noQueries) {
-    headers.queries = queries;
-  }
-  if (!noPrepend) {
-    let subDomain = 'kamino.spikeybot.com';
-    if (req.headers.host) {
-      subDomain = req.headers.host;
+    if (!noQueries) {
+      headers.queries = queries;
     }
-    req.url = `/${subDomain}${req.url}`;
-  }
-  common.log(`WS: ${req.headers.host}${req.url}`, ip);
-  proxy.ws(req, socket, head, {
-    target: {host: 'localhost', port: outputPort},
-    headers: headers,
+    if (!noPrepend) {
+      let subDomain = 'kamino.spikeybot.com';
+      if (req.headers.host) {
+        subDomain = req.headers.host;
+      }
+      req.url = `/${subDomain}${req.url}`;
+    }
+    common.log(`  : ${req.headers.host}${req.url}`, ip);
+    proxy.web(
+        req, res, {
+          target: {host: 'localhost', port: outputPort},
+          headers: headers,
+        },
+        (e) => {
+          if (e) {
+            common.error('Error in proxying request: ' + req.url, ip);
+            console.error(e);
+          }
+        });
   });
-});
-server.on('error', (e) => common.error(e));
-common.logDebug(`HTTPS Listening on port ${listenPort}`);
-common.logDebug(`Proxying to port ${outputPort}`);
-server.listen(listenPort, '::');
-const proxy = httpProxy.createProxyServer({ws: true});
-proxy.on('error', (e) => {
-  common.error(e.message);
-  if (e.message.indexOf('ENOTFOUND') == -1) console.log(e);
-});
+  server.on('upgrade', (req, socket, head) => {
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress ||
+        req.socket.remoteAddress || 'ERRR';
+
+    const headers = {'x-forwarded-for': ip};
+    let queries = '';
+    if (req.url.indexOf('?') >= 0) {
+      const split = req.url.split('?');
+      req.url = split[0];
+      queries = split[1];
+    }
+    if (!noQueries) {
+      headers.queries = queries;
+    }
+    if (!noPrepend) {
+      let subDomain = 'kamino.spikeybot.com';
+      if (req.headers.host) {
+        subDomain = req.headers.host;
+      }
+      req.url = `/${subDomain}${req.url}`;
+    }
+    common.log(`WS: ${req.headers.host}${req.url}`, ip);
+    proxy.ws(req, socket, head, {
+      target: {host: 'localhost', port: outputPort},
+      headers: headers,
+    });
+  });
+  server.on('error', (e) => common.error(e));
+  common.logDebug(`HTTPS Listening on port ${listenPort}`);
+  server.listen(listenPort, '::');
+  common.logDebug(`Proxying to port ${outputPort}`);
+  const proxy = httpProxy.createProxyServer({ws: true});
+  proxy.on('error', (e) => {
+    common.error(e.message);
+    if (e.message.indexOf('ENOTFOUND') == -1) console.log(e);
+  });
+}
 
 const portArg =
     process.argv.find((el) => el.startsWith('--verification-port='));
